0afc04d2bad02751e94e59777b449efe978a7773
Babar78 <muhammadbabarashraf8@gmail.com>
2025-06-27 23:47:58 +0500

    feat: add Mantine UI components and styles
    - Updated package.json and package-lock.json to include @mantine/core and @mantine/hooks.
    - Updated postcss configuration to include postcss-preset-mantine and postcss-simple-vars.
    - Modified index.jsx to integrate MantineProvider and apply Mantine theme.

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
+// Mantine
+import { createTheme, MantineProvider } from "@mantine/core";
+
+// Mantine Styles
+import "@mantine/core/styles.css";
+
 // Importing the router
 import { router } from "./Router.jsx";
 import { RouterProvider } from "react-router-dom";
@@ -11,11 +17,17 @@ import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
+const theme = createTheme({
+  cursorType: "pointer",
+});
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <QueryClientProvider client={queryClient}>
     {/* Router Provider */}
+    <MantineProvider theme={theme}>
     <RouterProvider router={router} />
+    </MantineProvider>
   </QueryClientProvider>
 );
 
