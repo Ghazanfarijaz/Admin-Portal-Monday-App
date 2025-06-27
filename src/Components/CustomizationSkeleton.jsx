85d33a5e42075bb2b124834296c58da5a18fcb1f
Babar78 <muhammadbabarashraf8@gmail.com>
2025-06-27 23:49:00 +0500

    feat: add CustomizationSkeleton component for improved loading state representation

---
+++ src/Components/CustomizationSkeleton.jsx
@@ -0,0 +1,49 @@
+import { Skeleton } from "@mantine/core";
+
+const CustomizationSkeleton = () => {
+  return (
+    <div className="bg-white rounded shadow-sm border border-gray-200 p-6 max-w-4xl flex flex-col gap-8">
+      {/* Board Section */}
+      <div className="flex flex-col gap-2">
+        <Skeleton width={100} height={25} radius={2} />
+        <Skeleton className="!w-full !max-w-[450px] !h-[48px] !rounded-lg" />
+      </div>
+
+      {/* Fields Section */}
+      <div className="flex flex-col gap-2">
+        <Skeleton width={100} height={25} radius={2} />
+        <div className="flex flex-col gap-4">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <Skeleton
+              key={index}
+              className="!w-full !max-w-[450px] !h-[48px] !rounded-lg"
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* Logo Section */}
+      <div className="flex flex-col gap-2">
+        <Skeleton width={50} height={25} radius={2} />
+        <Skeleton className="!w-[120px] !h-[120px] !rounded-lg" />
+      </div>
+
+      {/* Description Section */}
+      <div className="flex flex-col gap-2">
+        <Skeleton width={150} height={25} radius={2} />
+        <Skeleton className="!w-full !h-[100px]  !rounded-lg" />
+      </div>
+
+      {/* Sub domain */}
+      <div className="flex flex-col gap-2">
+        <Skeleton width={120} height={25} radius={2} />
+        <Skeleton className="!w-full !max-w-[450px] !h-[48px] !rounded-lg" />
+      </div>
+
+      {/* Button */}
+      <Skeleton className="!w-[170px] !h-[48px] !rounded-lg" />
+    </div>
+  );
+};
+
+export default CustomizationSkeleton;
