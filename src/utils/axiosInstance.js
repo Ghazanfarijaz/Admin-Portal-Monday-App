d406e7c55a1a3cafd5f2e6b6356d0ee10437407a
Babar78 <muhammadbabarashraf8@gmail.com>
2025-07-01 02:59:05 +0500

    fix: comment out local API base URL in axios instance configuration

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_BASE_URL =
   "https://c2c8a-service-23360785-a4c1a002.us.monday.app/api";
+// const API_BASE_URL =
+//   "http://localhost:8080/api";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
