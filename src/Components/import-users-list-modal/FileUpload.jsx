dd77abb15950666e340ab1f167ba14dc19414d48
Babar78 <muhammadbabarashraf8@gmail.com>
2025-07-18 22:21:55 +0500

    refactor: created_local_state_to_open_import_users_popup_instead_of_context_and_updated_skeleton

---
+++ src/Components/import-users-list-modal/FileUpload.jsx
@@ -0,0 +1,67 @@
+import { useRef, useState } from "react";
+import { Text } from "@vibe/core";
+import UploadDataIcon from "../../assets/icons/UploaddataIcon";
+import { parseExcelFile } from "../../utils/parsExcel";
+
+const FileUpload = ({ onUpload }) => {
+  const fileInputRef = useRef(null);
+  const [error, setError] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFile = async (file) => {
+    const validTypes = [
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "application/vnd.ms-excel",
+    ];
+
+    if (!file || !validTypes.includes(file.type)) {
+      setError("Please upload a valid Excel file (.xlsx or .xls)");
+      return;
+    }
+
+    try {
+      const parsedData = await parseExcelFile(file);
+      setSelectedFile(file);
+      setError("");
+      onUpload(parsedData);
+    } catch (err) {
+      console.error("Parse error:", err);
+      setError("Failed to parse Excel file.");
+    }
+  };
+
+  return (
+    <div
+      className="relative w-full h-[200px] flex items-center justify-center bg-[#E1EFF2] rounded-[4px] flex-col gap-3 cursor-pointer"
+      onClick={() => fileInputRef.current.click()}
+      onDragOver={(e) => e.preventDefault()}
+      onDrop={(e) => {
+        e.preventDefault();
+        handleFile(e.dataTransfer.files[0]);
+      }}
+    >
+      <UploadDataIcon className="text-[#007F9B] size-[40px]" />
+      <p className="text-[#007F9B] font-semibold text-[15px]/[160%] text-center">
+        Click to browse or <br />
+        drag and drop your Excel file
+      </p>
+      <input
+        ref={fileInputRef}
+        type="file"
+        accept=".xls,.xlsx"
+        onChange={(e) => handleFile(e.target.files[0])}
+        className="hidden"
+      />
+      {selectedFile && (
+        <Text className="text-sm text-gray-700 font-medium mt-2">
+          Selected: {selectedFile.name}
+        </Text>
+      )}
+      {error && (
+        <Text className="text-sm text-red-600 font-medium mt-2">{error}</Text>
+      )}
+    </div>
+  );
+};
+
+export default FileUpload;
