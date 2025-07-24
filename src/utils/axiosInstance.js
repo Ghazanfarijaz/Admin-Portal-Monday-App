6c9ae9bc3384e4b36a6055333dc99685a0f981fc
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2025-07-25 01:16:25 +0500

    Approved Specific User

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  "https://b09d6-service-23360785-a4c1a002.us.monday.app/api";
-// const API_BASE_URL = "http://localhost:8080/api";
+// const API_BASE_URL =
+//   "https://b09d6-service-23360785-a4c1a002.us.monday.app/api";
+const API_BASE_URL = "http://localhost:5000/api";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
