e4a66d80ea58d45c9c710d31703332144908aa92
Babar78 <muhammadbabarashraf8@gmail.com>
2025-08-28 21:23:31 +0500

    fix: update API base URL for axios instance

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Backend with no Secure Storage
 const API_BASE_URL =
-  "https://e8272-service-23360785-a4c1a002.us.monday.app/api";
+  "https://c7e66-service-23360785-a4c1a002.us.monday.app/api";
 
 // Backend with Secure Storage
 // const API_BASE_URL =
