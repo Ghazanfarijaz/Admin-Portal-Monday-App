1cff59108ff0d4e9a67679fe5540691ac6c86408
Babar78 <muhammadbabarashraf8@gmail.com>
2025-09-11 20:09:39 +0500

    feat: updated_live_url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Backend with no Secure Storage
 const API_BASE_URL =
-  "https://af7da-service-23360785-a4c1a002.us.monday.app/api";
+  "https://e301e-service-23360785-a4c1a002.us.monday.app/api";
 
 // Backend with Secure Storage
 // const API_BASE_URL =
