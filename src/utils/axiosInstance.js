ce4710d5951d3bacfeef2f0afe293595dcf6a071
Babar78 <muhammadbabarashraf8@gmail.com>
2025-09-17 20:02:25 +0500

    fix: update API base URL to correct endpoint

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  "https://bc008-service-23360785-a4c1a002.us.monday.app/api";
+  "https://f8cb9-service-23360785-a4c1a002.us.monday.app/api";
 
 // const API_BASE_URL = "http://localhost:8080/api";
 
